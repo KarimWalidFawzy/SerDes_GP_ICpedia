77220e36bcde0afecd6e7c3e66eaea19a881e2e8
OmarSalah239 <you@example.com>
2024-11-24 21:52:52 +0200

    changing names of the signals to be like the interface

--- hdl/tx/P2S.v
+++ hdl/tx/P2S.v
@@ -1,31 +1,31 @@
 module PISO(
-  input wire[9:0] parallel_in,
-  input wire clk,
-  input wire reset_n,
-  output reg serial_out
+  input wire[9:0] TxParallel_10,
+  input wire BitCLK,
+  input wire Reset,
+  output reg Serial
   );
 reg[9:0] temp_reg;  //temp_reg to hold the input parallel data
 reg [3:0] bit_count; 
 //shifting the input bits to serialize them 
-always @(posedge clk or negedge reset_n) begin
-  if(!reset_n)begin
+always @(posedge BitCLK or negedge Reset) begin
+  if(!Reset)begin
       temp_reg<=10'b0;
-      serial_out<=0;
+      Serial<=0;
   end 
   else begin
       if (bit_count == 0) begin
-      temp_reg<=parallel_in; 
-      serial_out<=parallel_in[0]; //this done to not stop a clk cycle loading the inputs as we need to load and transit at the same cycle (this done for the first bit (LSB))
+      temp_reg<=TxParallel_10; 
+      Serial<=TxParallel_10[0]; //this done to not stop a clk cycle loading the inputs as we need to load and transit at the same cycle (this done for the first bit (LSB))
       end
       else begin
-      serial_out<=temp_reg[1]; // this is done because we took the first bit (LSB) in the previous clk cycle
+        Serial<=temp_reg[1]; // this is done because we took the first bit (LSB) in the previous clk cycle
       temp_reg<=temp_reg>>1;
       end
   end
 end
 //couter to keep track the number of input bits
-always @(posedge clk or negedge reset_n) begin
-  if(!reset_n)begin
+always @(posedge BitCLK or negedge Reset) begin
+  if(!Reset)begin
       bit_count<=0;
   end 
   else begin
