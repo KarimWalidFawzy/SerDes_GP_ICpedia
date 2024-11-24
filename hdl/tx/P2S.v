8d950db1c626d1166eb7ddfe03c20150dfa3ddea
OmarSalah239 <you@example.com>
2024-11-24 22:21:11 +0200

    converting Serial from reg into wire

--- hdl/tx/P2S.v
+++ hdl/tx/P2S.v
@@ -2,40 +2,26 @@ module PISO(
   input wire[9:0] TxParallel_10,
   input wire BitCLK,
   input wire Reset,
-  output reg Serial
+  output wire Serial
   );
 reg[9:0] temp_reg;  //temp_reg to hold the input parallel data
-reg [3:0] bit_count; 
+reg [3:0] bit_count; //couter to keep track the number of input bits
 //shifting the input bits to serialize them 
 always @(posedge BitCLK or negedge Reset) begin
   if(!Reset)begin
       temp_reg<=10'b0;
-      Serial<=0;
-  end 
-  else begin
-      if (bit_count == 0) begin
-      temp_reg<=TxParallel_10; 
-      Serial<=TxParallel_10[0]; //this done to not stop a clk cycle loading the inputs as we need to load and transit at the same cycle (this done for the first bit (LSB))
-      end
-      else begin
-        Serial<=temp_reg[1]; // this is done because we took the first bit (LSB) in the previous clk cycle
-      temp_reg<=temp_reg>>1;
-      end
-  end
-end
-//couter to keep track the number of input bits
-always @(posedge BitCLK or negedge Reset) begin
-  if(!Reset)begin
       bit_count<=0;
   end 
   else begin
       if (bit_count == 0) begin
-      bit_count<=9;  
+      temp_reg<=TxParallel_10; 
+      bit_count<=9;     
       end
       else begin
+      temp_reg<=temp_reg>>1;
       bit_count<=bit_count-1;
       end
   end
 end
-
+assign Serial = temp_reg[0];
 endmodule
