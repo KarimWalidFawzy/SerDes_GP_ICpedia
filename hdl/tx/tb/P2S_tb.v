7049433c8d59094f1e68d3d459ece771ca847333
OmarSalah239 <you@example.com>
2024-11-24 22:27:03 +0200

    adding P2S_tb

---
+++ hdl/tx/tb/P2S_tb.v
@@ -0,0 +1,22 @@
+module tb();
+  reg[9:0] TxParallel_10;
+  reg BitCLK;
+  reg Reset;
+  wire Serial;
+  
+  always #5 BitCLK=~BitCLK;
+  PISO dut(.TxParallel_10(TxParallel_10),.BitCLK(BitCLK),.Reset(Reset),.Serial(Serial));
+  
+  initial begin
+    BitCLK=0;
+    Reset = 0;
+    TxParallel_10 = 10'b1001010010;
+    #20
+    Reset = 1;
+    TxParallel_10 = 10'b1001011011;
+    #100
+    TxParallel_10 = 10'b0111111110;
+    #100
+    TxParallel_10 = 10'b0101010101;
+    $finish;
+  end
